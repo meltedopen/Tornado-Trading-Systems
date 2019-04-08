d26d77cede81923a59d05e27581171162ab74a5a
Isaac <isaac.chang5@gmail.com>
2019-04-07 18:24:12 -0700

    pushing files

---
+++ search/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+    SearchProductView,
+)
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', SearchProductView.as_view(), name='query'),
+]
