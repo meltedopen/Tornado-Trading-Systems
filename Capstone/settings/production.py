34a39615f6fef9f6c94c06c062f30730d4620a6c
Isaac <isaac.chang5@gmail.com>
2019-04-10 23:30:59 -0700

    added postresql

--- Capstone/settings/production.py
+++ Capstone/settings/production.py
@@ -1,3 +1,4 @@
+import dj_database_url
 """
 Django settings for Capstone project.
 
@@ -95,6 +96,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
